97c168c4f4efcb42722c8d24f4a97ac1d836141c
johnaren <86240713+johnaren@users.noreply.github.com>
2022-12-01 12:48:28 +0100

    change back to create react root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
-import { render } from "react-dom"
+import ReactDOM from 'react-dom/client';
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
@@ -8,8 +7,8 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import AppView from './View/AppView';
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <AppView />
   </React.StrictMode>
