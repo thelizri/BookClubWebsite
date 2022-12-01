8c5a887471bdf375ce76afcf6436df8666bd2f15
johnaren <86240713+johnaren@users.noreply.github.com>
2022-12-01 14:18:21 +0100

    update app view

--- src/View/AppView.js
+++ src/View/AppView.js
@@ -1,12 +1,14 @@
-import ExampleComponent from "../Presenter/ExamplePresenter";
+// import ExampleComponent from "../Presenter/ExamplePresenter";
 // import LoginScreenView from "./LoginScreenView";
-// import NavigationBarView from "./NavigationBarView";
+import NavigationBarView from "./NavigationBarView";
 // import RegistrationPageView from "./RegistrationPageView";
-// import LandingPageView from "./LandingPageView";
+import LandingPageView from "./LandingPageView";
 
 function AppView() {
   return (
     <div>
+      <NavigationBarView />
+      <LandingPageView />
     </div>
   );
 }
