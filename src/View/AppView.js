94f1f0ca06d9464bfbfc7cee886c9ed2364dae8c
johnaren <86240713+johnaren@users.noreply.github.com>
2022-12-01 12:04:54 +0100

    clean app view

--- src/View/AppView.js
+++ src/View/AppView.js
@@ -1,12 +1,14 @@
 import ExampleComponent from "../Presenter/ExamplePresenter";
-import LoginScreenView from "./LoginScreenView";
-import NavigationBarView from "./NavigationBarView";
-import RegistrationPageView from "./RegistrationPageView";
-import LandingPageView from "./LandingPageView";
-
+// import LoginScreenView from "./LoginScreenView";
+// import NavigationBarView from "./NavigationBarView";
+// import RegistrationPageView from "./RegistrationPageView";
+// import LandingPageView from "./LandingPageView";
 
 function AppView() {
-  return <LandingPageView />;
+  return (
+    <div>
+    </div>
+  );
 }
 
 export default AppView;
