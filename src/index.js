b9c207d97d9e909e7e690c60d26f0a76a264493f
johnaren <86240713+johnaren@users.noreply.github.com>
2022-12-06 09:06:08 +0100

    change app component name

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
-
 import App from './Pages/AppView';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AppView />
-  </React.StrictMode>
+  <App/>
 );
