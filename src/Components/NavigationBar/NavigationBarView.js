8d7c1cfd34fc513ea5d1ba2dbb185dd0bcdd0379
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-12-06 11:29:40 +0100

    Added dropdown menu for smaller screens

--- src/Components/NavigationBar/NavigationBarView.js
+++ src/Components/NavigationBar/NavigationBarView.js
@@ -2,17 +2,30 @@ import React from 'react';
 import "./NavigationBarStyle.css";
 
 function NavigationBarView(props){
-    return (<nav className="navbar navbar-expand-lg navbar-light" id={"navigation-bar"}>
-        <a className="navbar-brand" href="src/components#"><span id={"name1"}>Yomu</span><span id={"name2"}>Bo</span></a>
-            <div className="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
-                <div className={"navbar-nav"}>
-                    <a className="nav-item nav-link active" href="src/components#" id={"navigation"}>Home</a>
-                    <a className="nav-item nav-link active" href="src/components#" id={"navigation"}>Clubs</a>
-                    <a className="nav-item nav-link active" href="src/components#" id={"navigation"}>Books</a>
-                    <a className="nav-item nav-link active" href={"#"} id={"navigation"}>About Us</a>
-                    <a className="nav-item nav-link active" href={"#"} id={"navigation"}>Profile</a>
-                </div>
-            </div>
+    return(<nav className="navbar navbar-light navbar-expand-lg" id={"navigation-bar"}>
+        <a href="src/components#" className="navbar-brand"><span id={"name1"}>Yomu</span><span id={"name2"}>Bo</span></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" >
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id={"navbarCollapse"}>
+            <ul className="navbar-nav ms-auto">
+                <li className="navbar-item">
+                    <a className="nav-link" href="src/components#" id={"navigation"}>Home</a>
+                </li>
+                <li className="navbar-item">
+                    <a className="nav-link" href="src/components#" id={"navigation"}>Clubs</a>
+                </li>
+                <li className="navbar-item">
+                    <a className="nav-link" href="src/components#" id={"navigation"}>Books</a>
+                </li>
+                <li className="navbar-item">
+                    <a className="nav-link" href={"#"} id={"navigation"}>About Us</a>
+                </li>
+                <li className="navbar-item">
+                    <a className="nav-link" href={"#"} id={"navigation"}>Profile</a>
+                </li>
+            </ul>
+        </div>
     </nav>);
 };
 
