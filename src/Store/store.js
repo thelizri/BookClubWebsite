5256424dbe648a6917a885858ec8c81f8e04c841
Azmeer K <azmeer.khawaja@outlook.com>
2022-12-06 12:07:06 +0100

    Added code for API functionality.

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../feature/authReducer";
+import { apiSlice } from "./api/";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
+/**
+ * Application state.
+ */
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
+        [apiSlice.reducerPath]: apiSlice.reducer, // api reducer
     },
-});
+
+    // API middleware for caching, invalidation, polling etc.
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware)
+
+})
+
+setupListeners(store.dispatch)
