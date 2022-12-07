2317d7a6f3d83e540a28cfa0aa86494ffbe601f0
Azmeer K <azmeer.khawaja@outlook.com>
2022-12-07 10:27:46 +0100

    Adjusted import reference.

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "./api/";
+import { googleBooksApi } from "./api/apiSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 /**
@@ -7,12 +7,12 @@ import { setupListeners } from "@reduxjs/toolkit/query";
  */
 export const store = configureStore({
     reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer, // api reducer
+        [googleBooksApi.reducerPath]: googleBooksApi.reducer, // api reducer
     },
 
     // API middleware for caching, invalidation, polling etc.
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(apiSlice.middleware)
+        getDefaultMiddleware().concat(googleBooksApi.middleware)
 
 })
 
