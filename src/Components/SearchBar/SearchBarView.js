225eaaa5ab20f2a03365efed62174388486f51bd
johnaren <86240713+johnaren@users.noreply.github.com>
2022-12-08 19:22:38 +0100

    change placeholder

--- src/Components/SearchBar/SearchBarView.js
+++ src/Components/SearchBar/SearchBarView.js
@@ -5,7 +5,7 @@ export default function SearchBarView(props) {
     // https://getbootstrap.com/docs/3.4/components/#input-groups
     return (
         <div className="input-group">
-            <input type="search" className="form-control rounded" placeholder="Search for books..." aria-label="Search"
+            <input type="search" className="form-control rounded" placeholder="Search..." aria-label="Search"
                    aria-describedby="search-addon"/>
             <button type="button" className="btn btn-outline-dark">search</button>
         </div>
