ab0550e66516efceb2b7e89edc83d6b981d90725
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-12-09 00:27:15 +0100

    Small tweaks

--- src/Components/ClubPanel/ClubPanelView.js
+++ src/Components/ClubPanel/ClubPanelView.js
@@ -1,5 +1,6 @@
 import ReadingList from "../ReadingList/ReadingList";
 import NextMeeting from "../NextMeeting/NextMeeting";
+import "./ClubPanelStyle.css";
 // DUMMY COMPONENTS
 var BookPanel = () => <div>[Book Panel Dummy]</div>;
 var VotingPanel = () => <div>[Voting Panel Dummy]</div>;
@@ -13,9 +14,9 @@ var MeetingPanel = () => <div>[Meeting Panel Dummy]</div>;
 export default function ClubPanelView(props) {
     return (
         // https://getbootstrap.com/docs/3.4/components/#panels
-        <div className="panel panel-default">
-            <div className="panel-header">{props.currentClub.name}</div>
-            <div className="panel-body">
+        <div className="panel panel-default" id={"panel"}>
+            <div className="panel-header" id={"panelhead"}>{props.currentClub.name}</div>
+            <div className="panel-body" id={"panelbody"}>
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8">
--- src/Components/NavigationBar/NavigationBarView.js
+++ src/Components/NavigationBar/NavigationBarView.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 export const NavigationBarView = function() {
     return(<nav className="navbar navbar-light navbar-expand-lg" id={"navigation-bar"}>
         <span id={"hiddenPadding"}>llll</span>
-        <a href="src/components#" className="navbar-brand"><span id={"name1"}>Yomu</span><span id={"name2"}>Bo</span></a>
+        <a href="src/components#" className="navbar-brand"><strong id={"name1"}>Yomu</strong><strong id={"name2"}>Bo</strong></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" >
             <span className="navbar-toggler-icon"></span>
         </button>
