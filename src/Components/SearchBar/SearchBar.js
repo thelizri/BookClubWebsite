32f75f8b899ef5715f4105689ad529e999388731
johnaren <86240713+johnaren@users.noreply.github.com>
2022-12-08 19:56:03 +0100

    add event firing

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,7 +1,11 @@
 import SearchBarView from "./SearchBarView";
 
+/**
+ * Search bar component
+ */
 const SearchBar = () => {
     return <SearchBarView
+        setSearchText = {(text) => console.log(text)}
         onSubmit = {(event) => console.log(event)}
     />;
 };
