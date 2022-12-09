42c13310d4807e46a92053e0c3134993bec89ee7
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-12-09 23:26:52 +0100

    Votes view

--- src/Components/Votes/VotesView.js
+++ src/Components/Votes/VotesView.js
@@ -17,7 +17,7 @@ function renderVotesCB(member){
     }
     return (<tr>
         <td>{member.name}</td>
-        <td style={{color: 'gray'}}>
+        <td style={{color: 'gray', opacity: 0.5}}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                  className="bi bi-check-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
