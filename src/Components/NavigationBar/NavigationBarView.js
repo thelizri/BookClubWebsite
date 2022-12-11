366f5c2c4f8345af709d099645c6ad238c82378d
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-12-11 23:35:17 +0100

    Routing

--- src/Components/NavigationBar/NavigationBarView.js
+++ src/Components/NavigationBar/NavigationBarView.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 export const NavigationBarView = function() {
     return(<nav className="navbar navbar-light navbar-expand-lg" id={"navigation-bar"}>
         <span id={"hiddenPadding"}>llll</span>
-        <a href="src/components#" className="navbar-brand"><strong id={"name1"}>Yomu</strong><strong id={"name2"}>Bo</strong></a>
+        <span className="navbar-brand"><strong id={"name1"}>Yomu</strong><strong id={"name2"}>Bo</strong></span>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" >
             <span className="navbar-toggler-icon"></span>
         </button>
@@ -17,10 +17,12 @@ export const NavigationBarView = function() {
                     </Link>
                 </li>
                 <li className="navbar-item">
-                    <a className="nav-link" href="src/components#" id={"navigation"}>Clubs</a>
+                    <Link to="/club" id={"react-link"}>
+                        <a className="nav-link" href="src/components#" id={"navigation"}>Clubs</a>
+                    </Link>
                 </li>
                 <li className="navbar-item">
-                    <Link to="/booksearch" id={"react-link"}>
+                    <Link to="/error" id={"react-link"}>
                         <a className="nav-link" href="src/components#" id={"navigation"}>Books</a>
                     </Link>
                 </li>
