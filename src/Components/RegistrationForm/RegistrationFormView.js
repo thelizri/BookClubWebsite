35cae2fb7e94c1de855eff44c8f02dbd4646433a
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-12-12 08:10:27 +0100

    Reg Form copied from persistance

--- src/Components/RegistrationForm/RegistrationFormView.js
+++ src/Components/RegistrationForm/RegistrationFormView.js
@@ -7,14 +7,22 @@ import Button from "react-bootstrap/Button";
 export const RegistrationFormView = function ({
                                                   error = "",
                                                   waiting = false,
+                                                  validEmail = () => {},
+                                                  validPassword = () => {},
                                                   onSubmit = (email, password, passwordConfirm) => {},
                                               }) {
+    const firstName = useRef();
+    const lastName = useRef();
+    const gender = useRef();
     const email = useRef();
     const password = useRef();
     const passwordConfirm = useRef();
 
     const handleSubmit = (event) => {
         onSubmit(
+            firstName.current.value,
+            lastName.current.value,
+            gender.current,
             email.current.value,
             password.current.value,
             passwordConfirm.current.value
@@ -25,32 +33,35 @@ export const RegistrationFormView = function ({
     return (<>
         <NavigationBarView />
         <div className={"container"}>
-            <Form>
+            <Form id={"registrationForm"}>
                 {/*First Name*/}
                 <Form.Group controlId="formRegFirstName" className={"m-2"}>
                     <Form.Label>First name</Form.Label>
-                    <Form.Control type="text"/>
+                    <Form.Control required type="text" ref={firstName}/>
                 </Form.Group>
 
                 {/*Last Name*/}
                 <Form.Group controlId="formRegLastName" className={"m-2"}>
                     <Form.Label>Last name</Form.Label>
-                    <Form.Control type="text"/>
+                    <Form.Control type="text" ref={lastName} />
                 </Form.Group>
 
                 {/*Gender*/}
                 <Form.Group controlId="formRegGender" className={"m-2"}>
-                    <div key={"inline-radio"}>
-                        <Form.Check inline name={"gender1"} type="radio" label="Male" id={"male-radio"} />
-                        <Form.Check inline name={"gender1"} type="radio" label="Female" id={"female-radio"} />
-                        <Form.Check inline name={"gender1"} type="radio" label="Other" id={"other-radio"} />
+                    <div key={"inline-radio"} onChange={event => gender.current = event.target.value}>
+                        <Form.Check inline name={"gender1"} value="Male" type="radio" label="Male" id={"male-radio"} />
+                        <Form.Check inline name={"gender1"} value="Female" type="radio" label="Female" id={"female-radio"} />
+                        <Form.Check inline name={"gender1"} value="Other" type="radio" label="Other" id={"other-radio"} />
                     </div>
                 </Form.Group>
 
                 {/*Email*/}
                 <Form.Group controlId="formRegEmail" className={"m-2"}>
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" ref={email}/>
+                    <Form.Control type="email" placeholder="Enter email" ref={email} isInvalid={validEmail()} />
+                    <Form.Control.Feedback type="invalid" className={"regFormEmailFeedback"}>
+                        Email error message
+                    </Form.Control.Feedback>
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -59,13 +70,16 @@ export const RegistrationFormView = function ({
                 {/*Password*/}
                 <Form.Group controlId="formRegPassword" className={"m-2"}>
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" ref={password}/>
+                    <Form.Control type="password" placeholder="Password" ref={password} isInvalid={validPassword()} />
+                    <Form.Control.Feedback type="invalid" className={"regFormPasswordFeedback"}>
+                        Password error message
+                    </Form.Control.Feedback>
                 </Form.Group>
 
                 {/*Password confirm*/}
                 <Form.Group controlId="formRegConfirmPassword" className={"m-2"}>
                     <Form.Label>Confirm password</Form.Label>
-                    <Form.Control type="password" placeholder="Confirm" ref={passwordConfirm}/>
+                    <Form.Control type="password" placeholder="Password" ref={passwordConfirm} />
                 </Form.Group>
 
                 {/*Submit button*/}
