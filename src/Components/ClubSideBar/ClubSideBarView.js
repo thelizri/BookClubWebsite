09ed93540503b536048804ca8531560ec6e9c472
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-12-13 15:47:36 +0100

    Made the sidebar look slightly nicer

--- src/Components/ClubSideBar/ClubSideBarView.js
+++ src/Components/ClubSideBar/ClubSideBarView.js
@@ -26,14 +26,14 @@ export default function ClubSideBarView(props) {
     return (
         // https://getbootstrap.com/docs/3.4/components/#panels
         <div>
-            <div className="panel panel-default" id={"sidebartop"}>
+            <div className="panel panel-default mt-5 mb-2" id={"sidebartop"}>
                 <div className="panel-heading">My Clubs</div>
                 <div className="panel-body">
                     <ClubList/>
                 </div>
             </div>
             <CreateClubModalView />
-            <div className="panel panel-default" id={"sidebarbottom"}>
+            <div className="panel panel-default mt-2" id={"sidebarbottom"}>
                 <div className="panel-heading">Find Clubs</div>
                 <div className="panel-body">
                     <SearchBar/>
