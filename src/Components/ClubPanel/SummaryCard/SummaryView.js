64e02ef7e759310fda8b4fab2a6d9089d44623e1
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-12-15 18:05:39 +0100

    Can edit meeting if admin

--- src/Components/ClubPanel/SummaryCard/SummaryView.js
+++ src/Components/ClubPanel/SummaryCard/SummaryView.js
@@ -62,7 +62,7 @@ const progressTab = (<div className={ "container bg-white m-2" }
 
 const synopsisTab = (
     <>
-        <h2>Free</h2>
+        <h2>Synopsis</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
         et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
         aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
