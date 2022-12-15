adae8962132e8bdf545c43a66b77ee41c3043fc0
johnaren <86240713+johnaren@users.noreply.github.com>
2022-12-15 21:43:45 +0100

    add popup with join button

--- src/Components/ClubSideBar/ClubSearch/ClubSearchResultsView.js
+++ src/Components/ClubSideBar/ClubSearch/ClubSearchResultsView.js
@@ -1,26 +1,65 @@
+/**
+ * Club Search Results View
+ *
+ * @param {foundClubs} - search results.
+ * @param {joinClub} - join club callback.
+ * @param {joinClub} - join club callback.
+ */
 const ClubSearchResultsView = ({
-   foundClubs,
-   onSubmit = (club) => {console.log(club)},
-   error,
+   foundClubs = [],
+   joinClub = (id) => console.log({joinClub: id}),
 }) => {
     function renderListItem(club) {
+        const ID = `club-search-results-${club.clubId}`;
+        const ModalID = ID + "-modal";
+        const ModalLabelID = ModalID + "-label";
+        
         return (
-            <li
-                className="list-group-item list-group-item-action h4 lead"
-                href="#"
-                onClick={ () => onSubmit(club) }
-                eventKey={ club.id }
-                id={ club.id }
-            > {
-                club.clubName
-            } </li>
+            <div>
+                <li
+                    className="list-group-item list-group-item-action"
+                    id={ID}
+                    href={"#" + ModalID}
+                    key={club.clubId}
+                    data-bs-toggle="modal"
+                >
+                    {club.clubName}
+                </li>
+                {/*Modal*/}
+                <div className="modal fade" id={ModalID} tabIndex="-1"
+                     aria-labelledby={ModalLabelID} aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header head-footer-modal">
+                                <h5 className="modal-title" id={ModalLabelID}>{club.clubName}</h5>
+                                <button type="button" className="btn-close"
+                                        data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                Body
+                            </div>
+                            <div className="modal-footer head-footer-modal">
+                                <button type="button"
+                                        className="btn btn-secondary close-modal"
+                                        data-bs-dismiss="modal">Close
+                                </button>
+                                <button type="button"
+                                        className="btn btn-primary"
+                                        onClick={() => joinClub(club.clubId)}
+                                >
+                                    Join Club
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 
     return (
-        <ul className="list-group"> {
-            foundClubs.map(renderListItem)
-        } </ul>
+        <ul className="list-group">{ foundClubs.map(renderListItem) }</ul>
     );
 }
 
