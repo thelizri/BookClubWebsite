761b9f678635c14bbe67752026d2905af09b95c6
johnaren <86240713+johnaren@users.noreply.github.com>
2022-12-15 18:29:44 +0100

    implement club list as accordion

--- src/Components/ClubSideBar/ClubList/ClubListView.js
+++ src/Components/ClubSideBar/ClubList/ClubListView.js
@@ -1,31 +1,67 @@
 import "./ClubListStyle.css"
+import Accordion from 'react-bootstrap/Accordion';
+import membersIcon from "../../../Images/members-icon.png";
+import languageIcon from "../../../Images/language-icon.png";
+import meetingIcon from "../../../Images/meeting-icon.png";
 
 /**
  * Displays a list of clubs.
+ *
+ * https://getbootstrap.com/docs/3.4/javascript/#collapse
  *
  * @param {Object} props -
  *      clubs: a list of clubs
  *      currentClub: the currently selected club
  *      setCurrentClub: callback for setting the currently selected club
  */
-export default function ClubListView( props ) {
-    // https://getbootstrap.com/docs/3.4/components/#list-group-linked
+export default function ClubListView({
+    clubs = [],
+    currentClub = null,
+    setCurrentClub = (club) => console.log({setCurrentClub: club}),
+}) {
     function renderListItem( club ) {
+        function renderInfo() {
+            const rows = [
+                [membersIcon, "Members", `${club.memberIds.length}/${club.maxMemberCount}`],
+                [languageIcon, "Language", club.language],
+                [meetingIcon, "Meetings", club.meetingType]
+            ];
+            
+            function renderInfoRow(icon, label, info) {
+                return (
+                    <tr>
+                        <td className="label">
+                            <img src={icon}/>
+                            {label}
+                        </td>
+                        <td className="info">{info}</td>
+                    </tr>
+                );
+            }
+            
+            return (
+                <table className="table">
+                    { rows.map((row) => renderInfoRow(row[0], row[1], row[2])) }
+                </table>
+            );
+        }     
+        
         return (
-            <a href="#" key={ club.name }
-               className={ "list-group-item list-group-item-action" }
-               onClick={ () => props.setCurrentClub( club ) }
-            >
-                { club.name }
-            </a>
+            <Accordion.Item eventKey={club.clubId}>
+                <Accordion.Header>
+                    { club.clubName }
+                </Accordion.Header>
+                <Accordion.Body>{renderInfo()}</Accordion.Body>
+            </Accordion.Item>
         );
     }
 
     return (
-        <div className="list-group" id={ "listForClubs" }>
-            {
-                props.clubs.map( renderListItem )
-            }
-        </div>
+        <Accordion
+            defaultActiveKey={currentClub.clubId}
+            flush
+        >
+            { clubs.map( renderListItem ) }
+        </Accordion>
     );
 }
