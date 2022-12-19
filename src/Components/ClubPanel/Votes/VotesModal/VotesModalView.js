791beb7b0765de25a33c149c8fbd76cf61546762
johnaren <86240713+johnaren@users.noreply.github.com>
2022-12-19 15:25:25 +0100

    update modal styles

--- src/Components/ClubPanel/Votes/VotesModal/VotesModalView.js
+++ src/Components/ClubPanel/Votes/VotesModal/VotesModalView.js
@@ -39,15 +39,15 @@ function VotesModalView({
             <div className="modal fade" id="votes-modal" tabIndex="-1" role="dialog" aria-labelledby="votes-modal-label">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
-                        <div className="modal-header">
-                            <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <div className="modal-header head-footer-modal">
                             <h4 className="modal-title" id="votes-modal-label">Voting Results</h4>
+                            <button type="button" className="btn btn-close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true"></span></button>
                         </div>
                         <div className="modal-body row m-1">
                             { isActive ? votingResults : "The vote deadline has not been reached yet."}
                         </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-default" data-bs-dismiss="modal">Close</button>
+                        <div className="modal-footer head-footer-modal">
+                            <button type="button" className="btn btn-secondary close-modal" data-bs-dismiss="modal">Close</button>
                         </div>
                     </div>
                 </div>
