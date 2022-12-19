e5bed0479f7128c0613dffd2cb8b36b277261d23
Azmeer K <azmeer.khawaja@outlook.com>
2022-12-19 15:10:01 +0100

    UI adjustments.

--- src/Components/ClubPanel/SummaryCard/SummaryView.js
+++ src/Components/ClubPanel/SummaryCard/SummaryView.js
@@ -1,25 +1,32 @@
 import React from "react";
 import "./SummaryStyle.css";
 
-const SummaryView = function ( { title, author = "", cover, summary = "No summary available :-/" } ) {
+const SummaryView = function( { title, author = "", cover,
+                                  summary = "No summary available :-/"
+                              } ) {
     return (
-        <div className={ "container bg-white m-2" }
-                id={ "summaryViewContainer" }>
-        <div className={ "row" }>
-            <div className={ "col" }>
-                <img src={ cover }
-                     style={ { width: 150 } }
-                     className={ "img-fluid" } alt={ "Image Missing" }
-                     id={ "imageSummary" }/>
-            </div>
-            <div className={ "col m-2" }>
-                <div><h4>{ title }</h4></div>
-                <div><h5 className={ "text-muted" }>{ author }</h5></div>
-                <br></br>
-                <div className="synopsis"><span dangerouslySetInnerHTML={ { __html: summary } } /></div>
+        <div><h1 id={ "header24ClubPanelView" }><br/></h1>
+            <div className={ "container bg-white m-2" }
+                 id={ "summaryViewContainer" }>
+                <div className={ "row" }>
+                    <div className={ "col" }>
+                        <img src={ cover }
+                             style={ { width : 150 } }
+                             className={ "img-fluid" } alt={ "Image Missing" }
+                             id={ "imageSummary" }/>
+                    </div>
+                    <div className={ "col m-2" }>
+                        <div><h5>{ title }</h5></div>
+                        <div><h6 className={ "text-muted" }>{ author }</h6>
+                        </div>
+                        <br></br>
+                        <div className="synopsis"><span
+                            dangerouslySetInnerHTML={ { __html : summary } }/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
     )
 };
 
